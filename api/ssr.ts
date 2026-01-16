161b1bb890ecd5d242138c5b7bbd071b7bbbf1a3
Raficate <rafaperezper@gmail.com>
2026-01-16 19:25:05 +0100

    Clean up custom Vercel config to use native Angular support

--- api/ssr.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Cargamos el servidor SSR ya compilado por Angular
-  // @ts-ignore
-  const { reqHandler } = await import('../dist/resolutions/server/server.mjs');
-  return reqHandler(req as any, res as any);
-}
